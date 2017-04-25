e7afccfd3be92d1f5ba759a70d45e7a271bcb703
Ava Pek <ava.pek@chaoticmoon.com>
2017-04-25 13:35:50 -0500

    Make GateNode smaller

--- DefaultNodes/Exec/GateNode.cs
+++ DefaultNodes/Exec/GateNode.cs
@@ -19,7 +19,7 @@ namespace Exodrifter.NodeGraph.DefaultNodes
 		internal ExecType execIn = ExecType.None;
 
 		[Description("True if the gate is open.")]
-		[SerializeField, Input("IsOpen", SocketFlags.Editable)]
+		[SerializeField, Input("IsOpen", 20, SocketFlags.Editable)]
 		internal bool isOpen = false;
 
 		[Description("The execution signal that passed through the gate.")]
