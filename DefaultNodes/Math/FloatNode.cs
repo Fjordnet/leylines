6976365a7be0e06ce6cd318c03d962f0c7b0747b
Ava Pek <ava.pek@chaoticmoon.com>
2017-06-06 16:19:21 -0500

    Add Float Node

---
+++ DefaultNodes/Math/FloatNode.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Exodrifter.NodeGraph.DefaultNodes
+{
+	[Node(Name = "Float", Path = "Math/Float")]
+	public class FloatNode : BakedNode
+	{
+		[Description("The value.")]
+		[SerializeField, Output("Value", SocketFlags.Editable | SocketFlags.AllowMultipleLinks)]
+		internal float value = 0;
+
+		public override float OutputWidth
+		{
+			get { return 40; }
+		}
+	}
+}
