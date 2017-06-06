44cd0e22868a5294bc3938d01152b3ae0f472496
Ava Pek <ava.pek@chaoticmoon.com>
2017-06-06 16:24:52 -0500

    Add math inequality nodes

---
+++ DefaultNodes/Math/EqualNode.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Exodrifter.NodeGraph.DefaultNodes
+{
+	[Node(Name = "Equal", Path = "Math/Equal")]
+	public class EqualNode : BakedNode
+	{
+		[Description("The left-hand value.")]
+		[SerializeField, Input("L")]
+		internal float l = 0;
+
+		[Description("The right-hand value.")]
+		[SerializeField, Input("R")]
+		internal float r = 0;
+
+		[Description("True if L and R are similar.")]
+		[SerializeField, Output("Result")]
+		internal bool result = false;
+
+		public override float InputWidth
+		{
+			get { return 40; }
+		}
+
+		#region Methods
+
+		public override void Eval(GraphScope scope)
+		{
+			result = Mathf.Approximately(l, r);
+		}
+
+		#endregion
+	}
+}
---
+++ DefaultNodes/Math/LessThanOrEqualNode.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Exodrifter.NodeGraph.DefaultNodes
+{
+	[Node(Name = "<=", Path = "Math/Less Than  Or Equal (<=)")]
+	public class LessThanOrEqualNode : BakedNode
+	{
+		[Description("The left-hand value.")]
+		[SerializeField, Input("L")]
+		internal float l = 0;
+
+		[Description("The right-hand value.")]
+		[SerializeField, Input("R")]
+		internal float r = 0;
+
+		[Description("The result of <i>L < R</i>.")]
+		[SerializeField, Output("Result")]
+		internal bool result = false;
+
+		public override float InputWidth
+		{
+			get { return 40; }
+		}
+
+		#region Methods
+
+		public override void Eval(GraphScope scope)
+		{
+			result = l <= r;
+		}
+
+		#endregion
+	}
+}
