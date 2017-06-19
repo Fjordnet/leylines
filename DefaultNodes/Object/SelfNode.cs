d8dc045345f02d3085668a1967ab68fcdf0fbb8d
Ava Pek <ava.pek@chaoticmoon.com>
2017-06-19 16:22:05 -0500

    Add transform to self node

--- DefaultNodes/Object/SelfNode.cs
+++ DefaultNodes/Object/SelfNode.cs
@@ -11,12 +11,16 @@ namespace Exodrifter.NodeGraph.DefaultNodes
 		[Description("The GameObject this node graph is on.")]
 		[SerializeField, Output("GameObject")]
 		internal GameObject gameObject = null;
+		[Description("The Transform this node graph is on.")]
+		[SerializeField, Output("Transform")]
+		internal Transform transform = null;
 
 		#region Methods
 
 		public override void Eval(GraphScope scope)
 		{
 			gameObject = scope.GameObject;
+			transform = gameObject.transform;
 		}
 
 		#endregion
